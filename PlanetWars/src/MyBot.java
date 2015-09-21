161ffc509ee35c93070fa30673116e84ef37e2a6
Anzhit <anchit.post@gmail.com>
2015-09-21 19:18:12 +0530

    Defense added

--- PlanetWars/src/MyBot.java
+++ PlanetWars/src/MyBot.java
@@ -1,7 +1,5 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class MyBot
 {
@@ -15,6 +13,120 @@ public class MyBot
 	// starting point, or you can throw it out entirely and replace it with
 	// your own. Check out the tutorials and articles on the contest website at
 	// http://www.ai-contest.com/resources.
+
+		static class TrialDefenseMission {
+	
+		public TrialDefenseMission (int planetID, int numShips) {
+			this.planetID = planetID;
+			this.numShips = numShips;
+		}
+		
+		public int PlanetID() {
+			return planetID;
+		}
+		
+		public int NumShips() {
+			return numShips;
+		}
+		  
+		private int planetID;
+		private int numShips;
+		
+	}
+	
+	static class PlanetReserve {
+	
+		public PlanetReserve (int planetID, int numShips) {
+			this.planetID = planetID;
+			this.numShips = numShips;
+		}
+		
+		public int PlanetID() {
+			return planetID;
+		}
+		
+		public int NumShips() {
+			return numShips;
+		}
+		
+		public void AddShips(int amount) {
+			numShips += amount;
+		}
+				
+		public void RemoveShips(int amount) {
+			numShips -= amount;
+		}
+		  
+		private int planetID;
+		private int numShips;
+		
+	}
+	
+	static class DefenseTasks implements Comparable<Object> {
+	
+		public DefenseTasks (int planetID, int turnsRemaining, int numShips) {
+			this.planetID = planetID;
+			this.turnsRemaining = turnsRemaining;
+			this.numShips = numShips;
+		}
+		
+		public int PlanetID() {
+			return planetID;
+		}
+		
+		public int TurnsRemaining() {
+			return turnsRemaining;
+		}
+		
+		public int NumShips() {
+			return numShips;
+		}
+		
+		public void RemoveShips(int amount) {
+			numShips -= amount;
+		}
+		
+		public int compareTo(Object obj) {
+			DefenseTasks anotherTask = (DefenseTasks) obj;
+			int anotherTaskTurn = anotherTask.TurnsRemaining();  
+			return this.turnsRemaining - anotherTaskTurn;
+		}
+		
+		private int planetID;
+		private int turnsRemaining;
+		private int numShips;
+		
+	}
+	
+	static class PlanetThreat implements Comparable<Object> {
+	
+		public PlanetThreat (int turnsRemaining, int numShips) {
+			this.turnsRemaining = turnsRemaining;
+			this.numShips = numShips;
+		}
+		
+		public int TurnsRemaining() {
+			return turnsRemaining;
+		}
+		
+		public int NumShips() {
+			return numShips;
+		}
+		
+		public void AddShips(int amount) {
+			numShips += amount;
+		}
+		
+		public int compareTo(Object obj) {
+			PlanetThreat anotherThreat = (PlanetThreat) obj;
+			int anotherThreatTurn = anotherThreat.TurnsRemaining();  
+			return this.turnsRemaining - anotherThreatTurn;
+		}
+  
+		private int turnsRemaining;
+		private int numShips;
+		
+	}
 
 	public static ArrayList<Planet> knapsack01(ArrayList<Planet> planets,
 			int maxWeight)
@@ -112,41 +224,150 @@ public class MyBot
 		else
 		{
 			// (1) If we currently have a fleet in flight, just do nothing.
-			if (pw.MyFleets().size() >= 1)
-			{
-				return;
-			}
-			// (2) Find my strongest planet.
-			Planet source = null;
-			double sourceScore = Double.MIN_VALUE;
-			for (Planet p : pw.MyPlanets())
-			{
-				double score = (double) p.NumShips();
-				if (score > sourceScore)
-				{
-					sourceScore = score;
-					source = p;
+		List<DefenseTasks> currentDefenseTasks = new ArrayList<DefenseTasks>();
+		List<PlanetReserve> currentPlanetReserves = new ArrayList<PlanetReserve>();
+		
+		for (Planet p : pw.MyPlanets()) {
+	
+			List<PlanetThreat> incomingThreats = new ArrayList<PlanetThreat>();
+			for (Fleet ef : pw.EnemyFleets()) {
+				if (p.PlanetID() == ef.DestinationPlanet()) {
+					int turnsRemaining = ef.TurnsRemaining();
+					int numShips = ef.NumShips();
+					boolean foundThreat = false;
+					for (PlanetThreat pt: incomingThreats) {
+						if (pt.TurnsRemaining() == turnsRemaining) {
+							pt.AddShips(numShips);
+							foundThreat = true;
+							break;
+						}
+					}
+					if (foundThreat == false) {
+						incomingThreats.add(new PlanetThreat(turnsRemaining, numShips));
+					}
 				}
 			}
-			// (3) Find the weakest enemy or neutral planet.
-			Planet dest = null;
-			double destScore = Double.MIN_VALUE;
-			for (Planet p : pw.NotMyPlanets())
-			{
-				double score = 1.0 / (1 + p.NumShips());
-				if (score > destScore)
-				{
-					destScore = score;
-					dest = p;
+			if (!incomingThreats.isEmpty()) {
+				Collections.sort(incomingThreats);
+				int turnProcessed = 0;
+				int startingExcessShips = 0;
+				int currentExcessShips = 0;
+				for (PlanetThreat pt: incomingThreats) {
+					int turnsRemaining = pt.TurnsRemaining();
+					int reinforcementShips = 0;
+					for (Fleet mf : pw.MyFleets()) {
+						if (p.PlanetID() == mf.DestinationPlanet() && mf.TurnsRemaining() > turnProcessed && mf.TurnsRemaining() <= turnsRemaining) {
+							reinforcementShips += mf.NumShips();
+						}
+					}
+					if (turnProcessed == 0) {
+						reinforcementShips += (turnsRemaining * p.GrowthRate());
+						if (reinforcementShips >= pt.NumShips()) {
+							startingExcessShips = p.NumShips();
+							currentExcessShips = (reinforcementShips - pt.NumShips());
+						}
+						else {
+							if ((reinforcementShips + p.NumShips()) >= pt.NumShips()) {
+								startingExcessShips = p.NumShips() - (pt.NumShips() - reinforcementShips);
+								currentExcessShips = startingExcessShips;
+							}
+							else {
+								int shortfall = pt.NumShips() - (reinforcementShips + p.NumShips());
+								currentDefenseTasks.add(new DefenseTasks(p.PlanetID(), turnsRemaining, shortfall));
+							}					
+						}
+					}
+					else {
+						int homeDefense = ((turnsRemaining - turnProcessed) * p.GrowthRate()) + currentExcessShips + reinforcementShips;
+						int shortfall = pt.NumShips() - homeDefense;
+						
+						if (shortfall > 0) {
+							currentDefenseTasks.add(new DefenseTasks(p.PlanetID(), turnsRemaining, shortfall));
+							currentExcessShips = 0;
+							startingExcessShips = currentExcessShips;
+							
+						}
+						else if (shortfall <= 0) {
+							currentExcessShips = homeDefense - pt.NumShips();
+							if (startingExcessShips > currentExcessShips) {
+								startingExcessShips = currentExcessShips;
+							}
+						}
+					}
+					turnProcessed = turnsRemaining;
+				}
+				if (startingExcessShips > 0) {
+					currentPlanetReserves.add(new PlanetReserve(p.PlanetID(), startingExcessShips));
 				}
 			}
-			// (4) Send half the ships from my strongest planet to the weakest
-			// planet that I do not own.
-			if (source != null && dest != null)
-			{
-				int numShips = source.NumShips() / 2;
-				pw.IssueOrder(source, dest, numShips);
+			else {
+				currentPlanetReserves.add(new PlanetReserve(p.PlanetID(), p.NumShips()));
 			}
+		}
+		
+		// try matching defense needs with available ships
+		// currently works on a first come, first served basis
+		// could be improved by prioritizing the defense of different planets based on their value
+		
+		Collections.sort(currentDefenseTasks);
+		int lastPlanetID = 100;
+		boolean taskUnfilled = false;
+		for (DefenseTasks dt : currentDefenseTasks) {
+			List<TrialDefenseMission> defenseMission = new ArrayList<TrialDefenseMission>();
+			int requiredShips = dt.NumShips();
+			if (lastPlanetID == dt.PlanetID() && taskUnfilled == true) {
+				continue;
+			}
+			lastPlanetID = dt.PlanetID();
+			
+			for (PlanetReserve pr : currentPlanetReserves) {
+				if ((pw.Distance(pr.PlanetID(), dt.PlanetID()) <= dt.TurnsRemaining()) && pr.NumShips() > 0 && requiredShips > 0) {
+					if (requiredShips >= pr.NumShips()) {
+						requiredShips -= pr.NumShips();
+						defenseMission.add(new TrialDefenseMission(pr.PlanetID(), pr.NumShips()));
+					}
+					else {
+						defenseMission.add(new TrialDefenseMission(pr.PlanetID(), requiredShips));
+						requiredShips = 0;
+					}
+				}
+			}
+			if (requiredShips == 0) {
+				for (TrialDefenseMission dm : defenseMission) {
+					pw.IssueOrder(pw.GetPlanet(dm.PlanetID()), pw.GetPlanet(dt.PlanetID()), dm.NumShips());
+					for (PlanetReserve pr : currentPlanetReserves) {
+						if (pr.PlanetID() == dm.PlanetID()) {
+							pr.RemoveShips(dm.NumShips());
+						}
+					}
+				}
+				taskUnfilled = false;
+				dt.RemoveShips(dt.NumShips());
+			}
+			else {
+				taskUnfilled = true;
+			}
+		}
+		
+		// if we can't defend a planet, we should consider abandoning it
+		
+		for (DefenseTasks dt : currentDefenseTasks) {
+			if (dt.NumShips() > 0 && dt.TurnsRemaining() == 1) {
+				boolean reserveFound = false;
+				Planet p = pw.GetPlanet(dt.PlanetID());
+				for (PlanetReserve pr : currentPlanetReserves) {
+					if (dt.PlanetID() == pr.PlanetID()) {
+						pr.AddShips(p.NumShips());
+						reserveFound = true;
+						break;
+					}
+				}
+				if (reserveFound == false) {	
+					currentPlanetReserves.add(new PlanetReserve(dt.PlanetID(), p.NumShips()));
+				}
+			}
+		}
+
 		}
 	}
 
