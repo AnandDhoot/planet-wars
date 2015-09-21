349c583693aa9c7c25414c29bdc8f1429cbf4199
Anzhit <anchit.post@gmail.com>
2015-09-21 14:35:42 +0530

    Starter Pack Added

--- MyBot.java
+++ MyBot.java
@@ -11,7 +11,55 @@ public class MyBot {
     // starting point, or you can throw it out entirely and replace it with
     // your own. Check out the tutorials and articles on the contest website at
     // http://www.ai-contest.com/resources.
-    public static void DoTurn(PlanetWars pw) {
+
+    public static  ArrayList<Planet> knapsack01(ArrayList<Planet> planets, int maxWeight) {
+            ArrayList<int> weights;
+            ArrayList<int> values;
+           
+            // solve 0-1 knapsack problem
+            for (int it = 0; it != planets.size(); ++it) {
+                // here weights and values are numShips and growthRate respectively
+                // you can change this to something more complex if you like...
+                Planet p = *it;       
+                weights.push_back(p.NumShips() + 1);
+                values.push_back(p.GrowthRate());
+            }
+           
+            int K[weights.size()+1][maxWeight];
+           
+            for (int i = 0; i < maxWeight; i++) {
+                K[0][i] = 0;
+            }
+           
+            for (int k = 1; k <= weights.size(); k++) {
+                for (int y = 1; y <= maxWeight; y++) {
+               
+                    if (y < weights[k-1]){
+                      K[k][y-1] = K[k-1][y-1];
+                    } 
+                    else if (y > weights[k-1]){
+                      K[k][y-1] = max( K[k-1][y-1], K[k-1][y-1-weights[k-1]] + values[k-1]);
+                    }
+                    else
+                      K[k][y-1] = max(K[k-1][y-1], values[k-1]);
+                }
+            }
+
+            // get the planets in the solution
+            int i = weights.size();
+            int currentW = maxWeight-1;
+            ArrayList<Planet> markedPlanets;
+           
+            while ((i > 0) && (currentW >= 0)) {
+                if (((i == 0) && (K[i][currentW] > 0)) || (K[i][currentW] != K[i-1][currentW])) {
+                    markedPlanets.push_back(planets[i-1]);
+                    currentW = currentW - weights[i-1];
+                }
+                i--;
+            }
+            return markedPlanets;
+    }
+ void DoTurn(PlanetWars pw) {
 	// (1) If we currently have a fleet in flight, just do nothing.
 	if (pw.MyFleets().size() >= 1) {
 	    return;
